016556d6f721b10dc8b7f2a6b88629eac37f1843
Abhijay Yadav <yadavabhijay@gmail.com>
2025-07-19 18:28:27 +0530

    started-dockerizing-server-app

---
+++ apps/server/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL to be ready..."
+until nc -z postgres 5432; do
+  sleep 1
+done
+
+echo "PostgreSQL is ready. Running migrations..."
+npx prisma migrate deploy
+npx prisma generate
+pnpm run build
+node ./dist/index.js
